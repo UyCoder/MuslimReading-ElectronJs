5af0255f29542851281cc1929b9e553adaaeef8a
uycoder <Thunder1987.>
2024-10-03 01:18:59 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile(path.join(__dirname, "src", "index.html"));
+}
+
+app.whenReady().then(createWindow);
